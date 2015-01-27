ef85dd46dd16ab55ffbfb4aff1c41230a8b889b4
eamoreno <edgaaaron@hotmail.com>
2015-01-26 20:37:22 -0600

    Se Agrego La Vista Para Visual
    La Interfaz Grafica Del Proyecto Se Genero

---
+++ LR1/Form1.Designer.cs
@@ -0,0 +1,218 @@
+﻿namespace LR1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben eliminar; false en caso contrario, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCarga = new System.Windows.Forms.Button();
+            this.rtbGramatica = new System.Windows.Forms.RichTextBox();
+            this.btnObtenTabla = new System.Windows.Forms.Button();
+            this.btnDibuja = new System.Windows.Forms.Button();
+            this.btnAnaliza = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.rtbGramaticaA = new System.Windows.Forms.RichTextBox();
+            this.gBGramatica = new System.Windows.Forms.GroupBox();
+            this.gBGramaticaA = new System.Windows.Forms.GroupBox();
+            this.btnGenera = new System.Windows.Forms.Button();
+            this.gBGramatica.SuspendLayout();
+            this.gBGramaticaA.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnCarga
+            // 
+            this.btnCarga.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnCarga.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCarga.Location = new System.Drawing.Point(51, 28);
+            this.btnCarga.Name = "btnCarga";
+            this.btnCarga.Size = new System.Drawing.Size(134, 41);
+            this.btnCarga.TabIndex = 0;
+            this.btnCarga.Text = "Carga Gramatica";
+            this.btnCarga.UseVisualStyleBackColor = false;
+            this.btnCarga.Click += new System.EventHandler(this.btnCarga_Click);
+            // 
+            // rtbGramatica
+            // 
+            this.rtbGramatica.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rtbGramatica.Location = new System.Drawing.Point(13, 21);
+            this.rtbGramatica.Name = "rtbGramatica";
+            this.rtbGramatica.Size = new System.Drawing.Size(140, 159);
+            this.rtbGramatica.TabIndex = 1;
+            this.rtbGramatica.Text = "";
+            // 
+            // btnObtenTabla
+            // 
+            this.btnObtenTabla.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnObtenTabla.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnObtenTabla.Location = new System.Drawing.Point(413, 62);
+            this.btnObtenTabla.Name = "btnObtenTabla";
+            this.btnObtenTabla.Size = new System.Drawing.Size(332, 41);
+            this.btnObtenTabla.TabIndex = 2;
+            this.btnObtenTabla.Text = "Obtener Tabla de Analisís Sintactico";
+            this.btnObtenTabla.UseVisualStyleBackColor = false;
+            // 
+            // btnDibuja
+            // 
+            this.btnDibuja.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnDibuja.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDibuja.Location = new System.Drawing.Point(51, 509);
+            this.btnDibuja.Name = "btnDibuja";
+            this.btnDibuja.Size = new System.Drawing.Size(332, 41);
+            this.btnDibuja.TabIndex = 3;
+            this.btnDibuja.Text = "Dibujar AFD a LR(0)";
+            this.btnDibuja.UseVisualStyleBackColor = false;
+            // 
+            // btnAnaliza
+            // 
+            this.btnAnaliza.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnAnaliza.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAnaliza.Location = new System.Drawing.Point(51, 391);
+            this.btnAnaliza.Name = "btnAnaliza";
+            this.btnAnaliza.Size = new System.Drawing.Size(134, 41);
+            this.btnAnaliza.TabIndex = 4;
+            this.btnAnaliza.Text = "Analizar";
+            this.btnAnaliza.UseVisualStyleBackColor = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(51, 336);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(148, 24);
+            this.textBox1.TabIndex = 5;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(413, 121);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(825, 220);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Tabla de Analisis Sintactico";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.groupBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox2.Location = new System.Drawing.Point(413, 391);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(825, 171);
+            this.groupBox2.TabIndex = 7;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Tabla de Acciones de Analisis Sintactico";
+            // 
+            // rtbGramaticaA
+            // 
+            this.rtbGramaticaA.Enabled = false;
+            this.rtbGramaticaA.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rtbGramaticaA.Location = new System.Drawing.Point(12, 36);
+            this.rtbGramaticaA.Name = "rtbGramaticaA";
+            this.rtbGramaticaA.Size = new System.Drawing.Size(134, 155);
+            this.rtbGramaticaA.TabIndex = 8;
+            this.rtbGramaticaA.Text = "";
+            // 
+            // gBGramatica
+            // 
+            this.gBGramatica.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.gBGramatica.Controls.Add(this.rtbGramatica);
+            this.gBGramatica.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gBGramatica.Location = new System.Drawing.Point(32, 90);
+            this.gBGramatica.Name = "gBGramatica";
+            this.gBGramatica.Size = new System.Drawing.Size(167, 210);
+            this.gBGramatica.TabIndex = 9;
+            this.gBGramatica.TabStop = false;
+            this.gBGramatica.Text = "Gramatica";
+            // 
+            // gBGramaticaA
+            // 
+            this.gBGramaticaA.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.gBGramaticaA.Controls.Add(this.rtbGramaticaA);
+            this.gBGramaticaA.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gBGramaticaA.Location = new System.Drawing.Point(223, 90);
+            this.gBGramaticaA.Name = "gBGramaticaA";
+            this.gBGramaticaA.Size = new System.Drawing.Size(160, 210);
+            this.gBGramaticaA.TabIndex = 10;
+            this.gBGramaticaA.TabStop = false;
+            this.gBGramaticaA.Text = "Gramatica Aumentada";
+            // 
+            // btnGenera
+            // 
+            this.btnGenera.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnGenera.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGenera.Location = new System.Drawing.Point(235, 28);
+            this.btnGenera.Name = "btnGenera";
+            this.btnGenera.Size = new System.Drawing.Size(134, 41);
+            this.btnGenera.TabIndex = 11;
+            this.btnGenera.Text = "Genera LR0";
+            this.btnGenera.UseVisualStyleBackColor = false;
+            this.btnGenera.Click += new System.EventHandler(this.btnGenera_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.ClientSize = new System.Drawing.Size(1330, 641);
+            this.Controls.Add(this.btnGenera);
+            this.Controls.Add(this.gBGramaticaA);
+            this.Controls.Add(this.gBGramatica);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnAnaliza);
+            this.Controls.Add(this.btnDibuja);
+            this.Controls.Add(this.btnObtenTabla);
+            this.Controls.Add(this.btnCarga);
+            this.Name = "Form1";
+            this.Text = "LR(0)";
+            this.gBGramatica.ResumeLayout(false);
+            this.gBGramaticaA.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCarga;
+        private System.Windows.Forms.RichTextBox rtbGramatica;
+        private System.Windows.Forms.Button btnObtenTabla;
+        private System.Windows.Forms.Button btnDibuja;
+        private System.Windows.Forms.Button btnAnaliza;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.RichTextBox rtbGramaticaA;
+        private System.Windows.Forms.GroupBox gBGramatica;
+        private System.Windows.Forms.GroupBox gBGramaticaA;
+        private System.Windows.Forms.Button btnGenera;
+    }
+}
+
--- LR1/Properties/AssemblyInfo.cs
+++ LR1/Properties/AssemblyInfo.cs
@@ -1,27 +1,36 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
 
-// Information about this assembly is defined by the following attributes.
-// Change them to the values specific to your project.
+// La información general sobre un ensamblado se controla mediante el siguiente 
+// conjunto de atributos. Cambie estos atributos para modificar la información
+// asociada con un ensamblado.
+[assembly: AssemblyTitle("LR1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("LR1")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
 
-[assembly: AssemblyTitle ("LR1")]
-[assembly: AssemblyDescription ("")]
-[assembly: AssemblyConfiguration ("")]
-[assembly: AssemblyCompany ("")]
-[assembly: AssemblyProduct ("")]
-[assembly: AssemblyCopyright ("diegorodriguez")]
-[assembly: AssemblyTrademark ("")]
-[assembly: AssemblyCulture ("")]
+// Si establece ComVisible como false, los tipos de este ensamblado no estarán visibles 
+// para los componentes COM. Si necesita obtener acceso a un tipo de este ensamblado desde 
+// COM, establezca el atributo ComVisible como true en este tipo.
+[assembly: ComVisible(false)]
 
-// The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
-// The form "{Major}.{Minor}.*" will automatically update the build and revision,
-// and "{Major}.{Minor}.{Build}.*" will update just the revision.
+// El siguiente GUID sirve como identificador de typelib si este proyecto se expone a COM
+[assembly: Guid("bd3164f2-ff35-417b-bf58-50b6b024ffec")]
 
-[assembly: AssemblyVersion ("1.0.*")]
-
-// The following attributes are used to specify the signing key for the assembly,
-// if desired. See the Mono documentation for more information about signing.
-
-//[assembly: AssemblyDelaySign(false)]
-//[assembly: AssemblyKeyFile("")]
-
+// La información de versión de un ensamblado consta de los cuatro valores siguientes:
+//
+//      Versión principal
+//      Versión secundaria 
+//      Número de compilación
+//      Revisión
+//
+// Puede especificar todos los valores o establecer como predeterminados los números de versión de compilación y de revisión 
+// mediante el asterisco ('*'), como se muestra a continuación:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
